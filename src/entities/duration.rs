fe07ca7d5f8da3eef5820498e31cb00d40573687
Rasmus Eneman <rasmus@eneman.eu>
2019-06-15 20:59:54 +0200

    test: Add duration tests for string parsing and arithmetic

--- src/entities/duration.rs
+++ src/entities/duration.rs
@@ -117,3 +117,147 @@ graphql_scalar!(Duration as "Duration" where Scalar = <S> {
       }
     }
 });
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_string_parse() {
+    assert_eq!(
+      Duration::from_string("1 minute"),
+      Some(Duration {
+        time_unit: TimeUnit::Minutes,
+        value: 1
+      })
+    );
+    assert_eq!(
+      Duration::from_string("2 minutes"),
+      Some(Duration {
+        time_unit: TimeUnit::Minutes,
+        value: 2
+      })
+    );
+    assert_eq!(
+      Duration::from_string("1 hour"),
+      Some(Duration {
+        time_unit: TimeUnit::Hours,
+        value: 1
+      })
+    );
+    assert_eq!(
+      Duration::from_string("5 hours"),
+      Some(Duration {
+        time_unit: TimeUnit::Hours,
+        value: 5
+      })
+    );
+    assert_eq!(
+      Duration::from_string("1 day"),
+      Some(Duration {
+        time_unit: TimeUnit::Days,
+        value: 1
+      })
+    );
+    assert_eq!(
+      Duration::from_string("10 days"),
+      Some(Duration {
+        time_unit: TimeUnit::Days,
+        value: 10
+      })
+    );
+    assert_eq!(
+      Duration::from_string("1 week"),
+      Some(Duration {
+        time_unit: TimeUnit::Weeks,
+        value: 1
+      })
+    );
+    assert_eq!(
+      Duration::from_string("12 weeks"),
+      Some(Duration {
+        time_unit: TimeUnit::Weeks,
+        value: 12
+      })
+    );
+    assert_eq!(
+      Duration::from_string("1 year"),
+      Some(Duration {
+        time_unit: TimeUnit::Years,
+        value: 1
+      })
+    );
+    assert_eq!(
+      Duration::from_string("42 years"),
+      Some(Duration {
+        time_unit: TimeUnit::Years,
+        value: 42
+      })
+    );
+  }
+
+  #[test]
+  fn test_minute_arithmetic() {
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) + &Duration::from_string("5 minutes").unwrap(),
+      Utc.ymd(2014, 7, 8).and_hms(9, 15, 11)
+    );
+
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) - &Duration::from_string("15 minutes").unwrap(),
+      Utc.ymd(2014, 7, 8).and_hms(8, 55, 11)
+    );
+  }
+
+  #[test]
+  fn test_hour_arithmetic() {
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) + &Duration::from_string("5 hours").unwrap(),
+      Utc.ymd(2014, 7, 8).and_hms(14, 10, 11)
+    );
+
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) - &Duration::from_string("15 hours").unwrap(),
+      Utc.ymd(2014, 7, 7).and_hms(18, 10, 11)
+    );
+  }
+
+  #[test]
+  fn test_day_arithmetic() {
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) + &Duration::from_string("5 days").unwrap(),
+      Utc.ymd(2014, 7, 13).and_hms(9, 10, 11)
+    );
+
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) - &Duration::from_string("15 days").unwrap(),
+      Utc.ymd(2014, 6, 23).and_hms(9, 10, 11)
+    );
+  }
+
+  #[test]
+  fn test_week_arithmetic() {
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) + &Duration::from_string("5 weeks").unwrap(),
+      Utc.ymd(2014, 8, 12).and_hms(9, 10, 11)
+    );
+
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) - &Duration::from_string("15 weeks").unwrap(),
+      Utc.ymd(2014, 3, 25).and_hms(9, 10, 11)
+    );
+  }
+
+  #[test]
+  fn test_year_arithmetic() {
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) + &Duration::from_string("5 years").unwrap(),
+      Utc.ymd(2019, 7, 7).and_hms(9, 10, 11)
+    );
+
+    assert_eq!(
+      Utc.ymd(2014, 7, 8).and_hms(9, 10, 11) - &Duration::from_string("15 years").unwrap(),
+      Utc.ymd(1999, 7, 12).and_hms(9, 10, 11)
+    );
+  }
+}
